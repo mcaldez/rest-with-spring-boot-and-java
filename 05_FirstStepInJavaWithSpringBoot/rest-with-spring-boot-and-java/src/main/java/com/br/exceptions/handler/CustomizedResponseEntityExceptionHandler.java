f5ab0d542405bb1a6641ddc0bd9147299344eaea
Belen La Patrona <mariabelencaldez@gmail.com>
2024-07-11 23:46:04 -0300

    adjusting exceptions, creating crud methods

--- 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/br/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/br/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.br.exceptions.ExceptionResponse;
-import com.br.exceptions.UnsuportedMathOperationException;
+import com.br.exceptions.ResourceNotFoundException;
 
 @RestController
 @ControllerAdvice
@@ -28,14 +28,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsuportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
 			Exception ex, WebRequest request){
 		ExceptionResponse exceptionResponse = new ExceptionResponse(
 				new Date(),
 				ex.getMessage(),
 				request.getDescription(false));
 		
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 }
