91612376c0b3332c97cf22114cbcc5c16059bb11
Belen La Patrona <mariabelencaldez@gmail.com>
2024-07-13 20:39:02 -0300

    implementing and configuring DozerMapper to implement Value Object pattern

--- 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/br/repositories/PersonRepository.java
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/br/repositories/PersonRepository.java
@@ -2,7 +2,7 @@ package com.br.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.br.model.Person;
+import com.br.data.vo.v1.PersonVO;
 
 
-public interface PersonRepository extends JpaRepository<Person, Long>{}
+public interface PersonRepository extends JpaRepository<PersonVO, Long>{}
