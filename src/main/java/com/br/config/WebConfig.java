2026a1617ae45ee8c21dcf0b950882e5a3699d14
Belen La Patrona <mariabelencaldez@gmail.com>
2024-07-15 17:55:45 -0300

    Media Type for Header param

--- src/main/java/com/br/config/WebConfig.java
+++ src/main/java/com/br/config/WebConfig.java
@@ -9,15 +9,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(true)
-		.parameterName("mediaType").ignoreAcceptHeader(true)
+	//Media Type por Query Param exemplo: http://localhost:8080/api/person/v1?mediaType=xml
+//	@Override
+//	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//		configurer.favorParameter(true)
+//		.parameterName("mediaType")
+//      .ignoreAcceptHeader(true)
+//		.useRegisteredExtensionsOnly(false)
+//		.defaultContentType(MediaType.APPLICATION_JSON)
+//		.mediaType("Json", MediaType.APPLICATION_JSON)
+//		.mediaType("xml", MediaType.APPLICATION_XML);
+//	}
+//	
+	//Media Type por header param exemplo: http://localhost:8080/api/person/v1
+		@Override
+		public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(false)
+        .ignoreAcceptHeader(false)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("Json", MediaType.APPLICATION_JSON)
 		.mediaType("xml", MediaType.APPLICATION_XML);
-	}
+		}
 
 	
 }
